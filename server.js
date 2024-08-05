7c945a0b8b5de24d513ef23a4ceee42f7d498a89
usk0318 <saikiranuppalapati2@gmail.com>
2024-08-05 15:07:39 +0530

    .

--- server.js
+++ server.js
@@ -10,27 +10,12 @@ const port1 = process.env.SERVER_PORT || 3000;
 // Connect to MongoDB
 connectDB();
 
-// Define User schema and model
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    // Add other fields as necessary
-});
-
-const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
-    res.send('Hello World pig');
+    res.send('Hello World');
 });
 
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
+
 
 app.listen(port1, () => {
     console.log('Server is running on port ' + port1);
